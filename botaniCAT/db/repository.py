d40a43bafd304a8717e2fe4f7089613f339450e1
Andrea Ros <androsrivas@outlook.com>
2025-09-21 19:41:14 +0200

    add functions CRUD for usos_medicinals having in mind N:M relationship

--- botaniCAT/db/repository.py
+++ botaniCAT/db/repository.py
@@ -26,7 +26,35 @@ def insert_nom_popular(conn, name, plant_id):
     cur.execute("INSERT INTO Noms_populars (name, plant_id) VALUES (?, ?)", (name, plant_id))
     conn.commit()
 
-def insert_us_medicinal(conn, description, plant_id):
+def get_us(conn, description):
     cur = conn.cursor()
-    cur.execute("INSERT INTO Usos_medicinals (description, plant_id) VALUES (?, ?)", (description, plant_id))
+    cur.execute("SELECT id FROM Usos_medicinals WHERE description=?", (description,))
+    row = cur.fetchone()
+    return row[0] if row else None
+
+def create_us(conn, description):
+    cur = conn.cursor()
+    cur.execute("INSERT OR IGNORE INTO Usos_medicinals (description) VALUES (?)", (description,))
     conn.commit()
+    return get_us(conn, description)
+
+def link_plant_us(conn, plant_id, us_id):
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT OR IGNORE INTO Plant_usos (plant_id, us_id) VALUES (?, ?)",
+        (plant_id, us_id)
+    )
+    conn.commit()
+    
+class Transaction:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def __enter__(self):
+        return self.conn.cursor()
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
